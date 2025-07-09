953914235b94128483ca1cb69d06b8ad320f03ee
ceekooo <sapna.f.2010@gmail.com>
2025-07-09 22:23:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple placeholder JS if needed for enhancements later
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! We'll get back to you shortly.");
+});
+
